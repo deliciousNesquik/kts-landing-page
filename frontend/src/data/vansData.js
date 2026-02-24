5ac0b3407ef082253f8d7067bc901d6f7dfc006b
deliciousNesquik <byte.in4matic@gmail.com>
2026-02-24 10:54:20 +0500

    feat(frontend): Добавить иконки мкр.автобусов.

--- frontend/src/data/vansData.js
+++ frontend/src/data/vansData.js
@@ -1,56 +1,63 @@
 export default function getVansData() {
     return [
         {
-            image: "/van_default.png",
+            image: "/vans/mercedes-sprinter.png",
             flags: null,
             title: "Mercedes Sprinter",
             //vehicle_class: "Микроавтобус",
             description: "Премиальный микроавтобус с высоким уровнем комфорта."
         },
         {
-            image: "/van_default.png",
+            image: "/vans/volkswagen_transporter.png",
             flags: null,
             title: "Volkswagen Transporter",
             //vehicle_class: "Микроавтобус",
             description: "Компактный и манёвренный микроавтобус с европейским качеством сборки. "
         },
         {
-            image: "/van_default.png",
+            image: "/vans/volkswagen-crafter.png",
+            flags: null,
+            title: "Volkswagen Crafter",
+            //vehicle_class: "Микроавтобус",
+            description: "Компактный и манёвренный микроавтобус с европейским качеством сборки. "
+        },
+        {
+            image: "/vans/ford-transit.png",
             flags: null,
             title: "Ford Transit",
             //vehicle_class: "Микроавтобус",
             description: "Надёжный и практичный микроавтобус с просторным салоном."
         },
         {
-            image: "/van_default.png",
+            image: "/vans/fiat-ducato.png",
             flags: null,
             title: "Fiat Ducato",
             //vehicle_class: "Микроавтобус",
             description: "Экономичный микроавтобус на платформе популярного коммерческого шасси."
         },
         {
-            image: "/van_default.png",
+            image: "/vans/peugeot-boxer.png",
             flags: null,
             title: "Peugeot Boxer",
             //vehicle_class: "Микроавтобус",
             description: "Экономичный микроавтобус на платформе популярного коммерческого шасси. "
         },
         {
-            image: "/van_default.png",
+            image: "/vans/citroen-jumper.png",
             flags: null,
             title: "Citroen Jumper",
             //vehicle_class: "Микроавтобус",
             description: "Экономичный микроавтобус на платформе популярного коммерческого шасси. "
         },
         {
-            image: "/van_default.png",
+            image: "/vans/gaz-next.png",
             flags: null,
             title: "Газ NEXT",
             //vehicle_class: "Микроавтобус",
             description: "Российский микроавтобус с адаптацией к местным условиям."
         },
         {
-            image: "/van_default.png",
+            image: "/vans/daily-furgone.png",
             flags: null,
             title: "Iveco Daily",
             //vehicle_class: "Микроавтобус",
